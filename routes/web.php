5d2d813800b74c0feae9ceebf570bb4fd13ff718
asrofilnadib <asrofilnadibs28@gmail.com>
2026-03-06 14:37:52 +0700

    feat: integrate Spatie Laravel Permission package and update Tailwind CSS and React versions

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,14 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/admin', function () {
+    return 'Hi Admin';
+})->middleware(['auth', 'role:admin'])->name('admin');
+
+Route::get('/user', function () {
+    return 'Hi User';
+})->middleware(['auth', 'role:user'])->name('user');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
