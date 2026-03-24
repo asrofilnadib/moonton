e0a1d36cef879d5eeeb3f2a9d360e930d81020e2
asrofilnadib <asrofilnadibs28@gmail.com>
2026-03-24 13:46:37 +0700

    feat: add CardMovies component for movie browsing in the Dashboard

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -2,6 +2,7 @@ import { Head } from "@inertiajs/react";
 import Flickity from "react-flickity-component";
 import Authenticated from "@/Layouts/Authenticated/Index";
 import FeatureMovies from "@/Layouts/Authenticated/FeatureMovies";
+import CardMovies from "@/Layouts/Authenticated/CardMovies";
 import PropTypes from "prop-types";
 
 PropTypes.FeatureMovies = {
@@ -33,7 +34,32 @@ export default function Dashboard() {
                 <div className="gap-[30px] __scroll-selector">
                     <Flickity options={flickityOptions}>
                         {[1, 2, 3, 4].map((i) => (
-                            <FeatureMovies key={i} slug={`/movies/${i}`} name={`Movie ${i}`} genre={`Genre ${i}`} rating={4.5} thumbnail={`/images/featured-${i % 2 === 0 ? "2" : "1"}.png`} />
+                            <FeatureMovies
+                                key={i}
+                                slug={`/movies/${i}`}
+                                name={`Movie ${i}`}
+                                genre={`Genre ${i}`}
+                                rating={4.5}
+                                thumbnail={`/images/featured-${i % 2 === 0 ? "2" : "1"}.png`}
+                            />
+                        ))}
+                    </Flickity>
+                </div>
+            </div>
+            <div className="mt-[50px]">
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Browse
+                </div>
+                <div className="gap-[30px]">
+                    <Flickity options={flickityOptions}>
+                        {[1, 2, 3, 4, 5, 6, 7].map((i) => (
+                            <CardMovies
+                                key={i}
+                                slug={`/movies/${i}`}
+                                name={`Movie ${i}`}
+                                genre={`Genre ${i}`}
+                                thumbnail={`/images/browse-${i % 2 === 0 ? "2" : "1"}.png`}
+                            />
                         ))}
                     </Flickity>
                 </div>
