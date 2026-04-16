32b409dc1809c683ade4b186f5c94c1405d22aee
asrofilnadib <asrofilnadibs28@gmail.com>
2026-04-16 19:02:37 +0700

    feat: implement CheckUserSubscription middleware to manage user access based on subscription status and update routes accordingly

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,7 +54,7 @@ class User extends Authenticatable
         return $this->hasMany(UserSubscription::class, 'user_id', 'id');
     }
 
-    public function getIsActiveSubscription()
+    public function getIsActiveAttribute()
     {
         if (! $this->lastActiveSubscription) {
             return false;
